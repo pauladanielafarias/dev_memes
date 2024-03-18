e329c462a61c39a13a9c72c4ca07936491d10cff
pauladanielafarias <pauladanielafarias@gmail.com>
2024-03-18 01:35:27 -0300

    style changes

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,6 @@
 <!-- ======= Footer ======= -->
 <footer id="footer">
-    <div class="container text-center m-5">
+    <div class="container text-center my-5">
         <p>Developed by Paula Farias</p>
         <a target="_blank" href=<?php echo $linkedin ?>><i class="bi bi-linkedin"></i></a>
         <a target="_blank" href=<?php echo $github ?>><i class="bi bi-github"></i></a>
